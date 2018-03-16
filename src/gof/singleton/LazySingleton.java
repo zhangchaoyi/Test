aaea87600935342dbc0ca8fb882c0fa4fc12a531
chris <zhangchaoyi@yuntutv.net>
2018-03-16 11:09:32 +0800

    添加延迟加载单例

--- src/gof/singleton/LazySingleton.java
+++ src/gof/singleton/LazySingleton.java
@@ -7,6 +7,17 @@ public class LazySingleton {
 
     private LazySingleton(){}
 
-    private static LazySingleton
+    private static volatile LazySingleton lazySingleton = null;
+
+    public static LazySingleton getLazySingleton(){
+        if(lazySingleton == null){
+            synchronized(LazySingleton.class) {
+                if(lazySingleton == null){
+                    lazySingleton = new LazySingleton();
+                }
+            }
+        }
+        return lazySingleton;
+    }
 }
 
