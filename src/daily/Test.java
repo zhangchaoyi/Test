1d4254eda8eacb4e8a9f70e2c73c041aa5cc6f98
chris <150299591@qq.com>
2018-03-25 20:21:50 +0800

    add & ^ |

---
+++ src/daily/Test.java
@@ -0,0 +1,18 @@
+package daily;
+
+/**
+ * Created by chris on 3/25/18.
+ * 位运算优先级  & ^ |
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        System.out.println(8|9&10^11);
+        System.out.println(9&10);
+        System.out.println(8^11);
+        System.out.println(8|3);
+
+
+
+    }
+}
