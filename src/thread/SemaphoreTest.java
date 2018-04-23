60f275d8317c275794248d210ceef0e1118e4ad2
chris <zhangchaoyi@yuntutv.net>
2018-04-23 17:24:20 +0800

    添加semaphore test

---
+++ src/thread/SemaphoreTest.java
@@ -0,0 +1,33 @@
+package thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zcy on 18-4-23.
+ */
+public class SemaphoreTest {
+
+    public static void main(String[] args) throws Exception{
+        Semaphore s = new Semaphore(3);
+
+        for(int i = 0 ; i<5; i++){
+            s.acquire(1);
+            final int fi = i;
+            Thread t = new Thread(() -> run(fi));
+            t.start();
+        }
+
+    }
+
+    public static void run(int fi) {
+        while(true) {
+            System.out.println(fi);
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
