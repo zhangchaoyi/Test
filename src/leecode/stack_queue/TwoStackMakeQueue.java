8d8329df48765ed527640566dba8c002acdf0297
zhangchaoyi <chaoyi.zhang@zatech.com>
2019-10-11 17:03:37 +0800

    update

--- src/leecode/stack_queue/TwoStackMakeQueue.java
+++ src/leecode/stack_queue/TwoStackMakeQueue.java
@@ -1,7 +1,67 @@
 package leecode.stack_queue;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 /**
  * 两个栈实现一个队列
+ * 一个栈stackIn 用于入栈使用
+ * 一个栈stackOut 用于出栈使用，如果stackOut为空，则将stackIn中所有数据出栈迁移stackOut，再将stackOut出栈
  */
 public class TwoStackMakeQueue {
+
+    private Stack<Number> stackIn = new Stack<>();
+    private Stack<Number> stackOut = new Stack<>();
+
+    public boolean push(Number n){
+        stackIn.push(n);
+        return true;
+    }
+
+    public Number pop() {
+        if(stackOut.isEmpty()){
+            while(!stackIn.isEmpty()){
+                stackOut.push(stackIn.pop());
+            }
+        }
+
+        return stackOut.pop();
+    }
+
+    @Override public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("stackIn:[");
+        if(!stackIn.isEmpty()){
+            Iterator<Number> iterator = stackIn.iterator();
+            while(iterator.hasNext()){
+                sb.append(iterator.next() + " ");
+            }
+        }
+        sb.append("]");
+
+        sb.append("\n stackOut:[");
+        if(!stackOut.isEmpty()){
+            Iterator<Number> iterator = stackOut.iterator();
+            while(iterator.hasNext()){
+                sb.append(iterator.next() + " ");
+            }
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        TwoStackMakeQueue q = new TwoStackMakeQueue();
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+        q.push(6);
+
+        q.pop();
+        q.pop();
+
+    }
 }
--- src/leecode/stack_queue/reverseStackWithRecursion.java
+++ src/leecode/stack_queue/reverseStackWithRecursion.java
@@ -1,10 +1,44 @@
 package leecode.stack_queue;
 
+import java.util.Stack;
+
 /**
- * 使用递归逆序一个栈
+ * 使用递归逆序一个栈, 即将栈中的元素完全反转
  * 思路：设计一个递归函数getAndRemoveLastElement获取栈底元素并删除
  *      设计一个递归函数reverse
  *
  */
 public class reverseStackWithRecursion {
+
+    public static Number getAndRemoveLastElement(Stack<Number> stack){
+        Number n = null;
+        if(stack.size() == 1){
+            return stack.pop();
+        } else {
+            n = stack.pop();//要将stack的元素出栈，size才能减1，递归才能进行；同时弹出的stack元素在递归的栈内存中
+        }
+        Number last = getAndRemoveLastElement(stack);
+        stack.push(n);
+
+        return last;
+    }
+
+    public static void reverse(Stack<Number> stack){
+        if (stack.isEmpty()) {
+            return;
+        }
+        Number last = getAndRemoveLastElement(stack);
+        reverse(stack);
+        stack.push(last);
+    }
+
+    public static void main(String[] args){
+        Stack<Number> stack = new Stack<Number>(){
+            {push(1);push(2);push(3);push(4);push(5);}
+        };
+
+        reverse(stack);
+
+        System.out.println("==================");
+    }
 }
