18c29b1249058f01ecfc332bcba681463dbe5c8f
张超逸 <zhangchaoyi@zhangchaoyideMacBook-Pro.local>
2019-10-21 23:58:58 +0800

    update leecode

--- src/leecode/DynamicPlanning/Fibonacci.java
+++ src/leecode/DynamicPlanning/Fibonacci.java
@@ -15,4 +15,47 @@ public class Fibonacci {
     public static void main(String[] args){
 
     }
+
+    /**
+     * 求矩阵m的p次方
+     * @param m
+     * @param p
+     * @return
+     */
+    public int[][] matrixPower(int[][] m, int p){
+        int[][] res = new int[m.length][m[0].length];
+
+        //先把res设为单位矩阵，相当于整数中的1
+        for(int i=0;i<res.length;i++){
+            res[i][i] = 1;
+        }
+        int[][] tmp = m;
+        for(;p!=0;p>>=1){
+            if((p&1)!=0){
+                res = muliMatrix(res, tmp);
+            }
+            tmp = muliMatrix(tmp, tmp);
+        }
+        return tmp;
+    }
+
+    /**
+     * 两个矩阵相乘
+     * @param m1
+     * @param m2
+     * @return
+     */
+    public static int[][] muliMatrix(int[][] m1, int[][] m2){
+        int[][] res = new int[m1.length][m2[0].length];
+
+        for(int i=0;i<m1.length;i++){
+            for(int j=0;j<m2[0].length;j++){
+                for(int k=0;k<m2.length;k++){
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+
+        return res;
+    }
 }
