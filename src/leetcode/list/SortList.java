34f632348c7f688d5593edcfb5e121c34f9d46e0
zhangchaoyi <chaoyi.zhang@za.group>
2020-08-28 15:26:08 +0800

    add sort list

---
+++ src/leetcode/list/SortList.java
@@ -0,0 +1,25 @@
+package leetcode.list;
+
+/**
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ */
+public class SortList {
+
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
