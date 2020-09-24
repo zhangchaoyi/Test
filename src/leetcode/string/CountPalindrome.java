c99e222ae6ba8e6db56834da477809b0e11b5b24
zhangchaoyi <chaoyi.zhang@za.group>
2020-09-24 18:17:21 +0800

    添加回文子串统计

---
+++ src/leetcode/string/CountPalindrome.java
@@ -0,0 +1,33 @@
+package leetcode.string;
+
+/**
+ * 647. 回文子串
+ *
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ *
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+ *
+ * 输入："abc"
+ * 输出：3
+ * 解释：三个回文子串: "a", "b", "c"
+ *
+ *
+ * 输入："aaa"
+ * 输出：6
+ * 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+ *
+ *
+ * 1.从中心拓扑，因为回文的特性是两边字符一样，需要区分奇偶
+ * 时间复杂度O(n^2)
+ *
+ * 2.dp
+ */
+public class CountPalindrome {
+
+    public int countSubstrings(String s) {
+//        for(){}
+        return 0;
+    }
+
+    public static void main(String[] args){}
+}
