f46e39652f5e9ae495ae49b8d5874ac330cedb3f
zhangchaoyi <chaoyi.zhang@za.group>
2020-09-25 15:36:02 +0800

    回文数添加 奇偶统一

--- src/leetcode/string/CountPalindrome.java
+++ src/leetcode/string/CountPalindrome.java
@@ -16,7 +16,7 @@ package leetcode.string;
  * 输出：6
  * 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
  *
- *
+ * 如果先找出所有的子串再判断是否回文，时间复杂度O(n^3)
  * 1.从中心拓扑，因为回文的特性是两边字符一样，需要区分奇偶
  * 时间复杂度O(n^2)
  *
@@ -71,9 +71,41 @@ public class CountPalindrome {
         }
     }
 
+    /**
+     * 把 n*2 统一处理奇偶的回文中心
+     * 编号 ii	回文中心左起始位置 l_il
+     * i
+     * ​
+     *  	回文中心右起始位置 r_ir
+     * i
+     * ​
+     * i  left right
+     * 0	0	0
+     * 1	0	1
+     * 2	1	1
+     * 3	1	2
+     * 4	2	2
+     * 5	2	3
+     * 6	3	3
+     * @param s
+     * @return
+     */
+    public int countSubstringsUnite(String s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < 2 * n - 1; ++i) {
+            int l = i / 2, r = i / 2 + i % 2;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                --l;
+                ++r;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         String s = "abc";
         CountPalindrome countPalindrome = new CountPalindrome();
-        System.out.println(countPalindrome.countSubstrings(s));
+        System.out.println(countPalindrome.countSubstringsUnite(s));
     }
 }
