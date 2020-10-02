8179032c4f6bddc9aef6025d83b98e024c5ead5e
chaoyi.zhang <chaoyi.zhang@za.group>
2020-10-02 11:12:27 +0800

    找两个有序数组的中间元素 O(m+n)

--- src/leetcode/array/MedianSortedArray.java
+++ src/leetcode/array/MedianSortedArray.java
@@ -1,5 +1,6 @@
 package leetcode.array;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,35 +25,53 @@ import java.util.List;
 public class MedianSortedArray {
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 2};
-        int[] nums2 = new int[]{3, 4};
+        int[] nums1 = new int[]{2};
+        int[] nums2 = new int[]{};
 
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 
+    /**
+     * 二路归并进行插入新数组
+     * O(m+n)
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         List<Integer> temp = new ArrayList<>();
 
         int i=0;//for nums1
         int j=0;//for nums2
-        int m = (nums1.length + nums2.length)/2;
+        int length = nums1.length + nums2.length;
+        boolean even = (length % 2 == 0);
+        int mid = (nums1.length + nums2.length)/2;
 
-        while((i + j) < m ){
+        while((i + j) <= mid ){
             //分别判断 i j 溢出
             if(i > nums1.length - 1){
-                temp.add(nums2[j]);
+                temp.add(nums2[j++]);
+                continue;
             }
             if(j > nums2.length - 1){
-                temp.add(nums1[i]);
+                temp.add(nums1[i++]);
+                continue;
             }
 
             if(nums1[i] < nums2[j]){
-                temp.add(nums1[i]);
+                temp.add(nums1[i++]);
             } else {
-                temp.add(nums2[j]);
+                temp.add(nums2[j++]);
             }
         }
 
-        return temp.get(temp.size() - 1);
+        if(even){
+            BigDecimal bigDecimal1 = new BigDecimal(temp.get(temp.size() - 1) + temp.get(temp.size() - 2));
+            BigDecimal bigDecimal2 = bigDecimal1.divide(new BigDecimal(2));
+            return bigDecimal2.doubleValue();
+        } else {
+            return temp.get(temp.size() - 1);
+        }
+
     }
 }
