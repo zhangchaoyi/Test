b14e0f285e033f142b8fc12de60d3e5bfdb008d2
chaoyi.zhang <chaoyi.zhang@za.group>
2020-10-09 10:46:45 +0800

    添加最长有效括号长度

--- src/leetcode/DynamicPlanning/LongestValidParentheses.java
+++ src/leetcode/DynamicPlanning/LongestValidParentheses.java
@@ -1,8 +1,5 @@
 package leetcode.DynamicPlanning;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 /**
  * 32. 最长有效括号
  *给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
@@ -20,40 +17,60 @@ import java.util.Objects;
  *
  * 暴力破解O(n^2)
  *
+ * 假设dp[i]为以i结尾的有效括号子串长度，则只需找出dp[]的 max
+ *
+ * 状态方程：
+ * 则对于 i='(' , dp[i]=0
+ * 对于 i=')' , 如果前一位是'(' , 则dp[i]=dp[i-2]+2 ;
+ *              如果前一位是')' , 则位置i的对应左括号位置是 i-dp[i-1]-1 , 要满足括号有效子串，要求 s[i-dp[i-1]-1] == '(' , 此时 dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]，   -- dp[i-dp[i-1]-2]是之前的独立有效括号段
+ *
+ *
  *
  */
 public class LongestValidParentheses {
 
     public static void main(String[] args){
-        String s = ")()())";
-        char[] chars = s.toCharArray();
-        //maxLength[i]表示以i开头的最长有效括号长度
-        int[] maxLength = new int[chars.length];
-        int maxLen = 0;
-        for(int i=0;i<chars.length;i++){
-            char last = chars[i];
-            if (Objects.equals(last, ')')) {
-                maxLength[i]=0;
-                continue;
-            }
-            int curMaxLen = 1;
-            for(int j=i+1;j<chars.length;j++){
-                if (Objects.equals(last, '(') && Objects.equals(chars[j], ')')) {
-                    curMaxLen++;
-                } else if(Objects.equals(last, ')') && Objects.equals(chars[j], '(') && curMaxLen > 0){
-                    curMaxLen++;
-                }
-                last = chars[j];
-            }
-            if(curMaxLen > maxLen){
-                maxLen = curMaxLen;
-            }
-            maxLength[i]=curMaxLen;
-        }
+        String s = "(()";
+        LongestValidParentheses lvp = new LongestValidParentheses();
+        System.out.println(lvp.longestValidParentheses(s));
 
-        System.out.println(Arrays.toString(maxLength));
-        System.out.println(maxLen);
     }
 
+    public int longestValidParentheses(String s){
+        int[] dp = new int[s.length()];
+        char[] chars = s.toCharArray();
+        for(int i=0;i<chars.length;i++){
+            if (chars[i]=='(') {
+                dp[i] = 0;
+                continue;
+            }
+            if(chars[i]==')' && i>1){
+                //判断前一位
+                if(chars[i-1]=='('){
+                    dp[i]=dp[i-2]+2;
+                } else {
+                    if(chars[i-dp[i-1]-1]=='('){
+                        dp[i] = dp[i-1]+2 + dp[i-dp[i-1]-2];
+                    }
+                }
+            } else {
+                if (i==0) {
+                    dp[0]=0;
+                } else if(i==1){
+                    if(chars[0]=='('){
+                        dp[1]=2;
+                    }
+                }
+            }
+        }
+        //找出dp中的最大值
+        int max = dp[0];
+        for(int i=1;i<dp.length;i++){
+            if (dp[i]>max) {
+                max=dp[i];
+            }
+        }
+        return max;
+    }
 
 }
