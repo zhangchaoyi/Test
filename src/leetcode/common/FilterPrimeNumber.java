3c58dd5e7651b3948e701449185312666025008f
zhangchaoyi <chaoyi.zhang@za.group>
2020-10-09 18:18:28 +0800

    add rob2

--- src/leetcode/common/FilterPrimeNumber.java
+++ src/leetcode/common/FilterPrimeNumber.java
@@ -11,14 +11,13 @@ import java.util.List;
  * 2.普通筛， 根据 "一个素数的倍数一定是合数", 从i=2开始筛除 i*2 , 缺点是存在重复筛除  O(n*loglogn)
  *
  * 3.线性筛除素数 O(n)
- * 对于一个数n(无论质数或者合数)，一次循环筛除所有小于n的最小质因子的所有质数 * n
- *  比如对于 55 = 5 * 11 ， 筛除 2 * 55 、 3 * 55 、 5 * 55
+ * 对于一个数n(无论质数或者合数)，一次循环筛除所有小于n的最小质因子的所有质数 * i
  *
  */
 public class FilterPrimeNumber {
 
     public static void main(String[] args){
-        System.out.println(getPrimeNumber(20));
+        System.out.println(getPrimeNumber(100));
     }
 
     public static List<Integer> getPrimeNumber(int n){
@@ -41,7 +40,7 @@ public class FilterPrimeNumber {
                  * 由于primeNumbers是递增，因此如果 i 能整除小的质数，可以跳出
                  *
                  *  当 i % primeNumbers[j] == 0,
-                 *  考虑 i * primeNumbers[j+1] = k * primeNumbers[j] * primeNumbers[j+1]
+                 *  考虑 i * primeNumbers[j+1] = (k * primeNumbers[j]) * primeNumbers[j+1]
                  *                             = m * primeNumbers[j];
                  *   因为 primeNumbers[j] < primeNumbers[j+1]   , 因此必定存在一个 m > i
                  */
