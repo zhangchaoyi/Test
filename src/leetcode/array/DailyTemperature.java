c0c3017c96d25ed28dcef80e8d0efcce43bf90da
chaoyi.zhang <chaoyi.zhang@za.group>
2020-10-10 17:02:04 +0800

    每日温度 完成单调栈

--- src/leetcode/array/DailyTemperature.java
+++ src/leetcode/array/DailyTemperature.java
@@ -1,6 +1,7 @@
 package leetcode.array;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * 739. 每日温度
@@ -17,7 +18,9 @@ import java.util.Arrays;
  *
  * 1.暴力解法 O(N^2)
  *
- * 2.单调栈
+ * 2.单调栈 O(n), + 空间复杂度O(n)
+ * 栈中保存元素下标，每当数组元素 num[i] > stack[topIndex] , 进行弹栈，并计算栈元素索引的下标差  i-topIndex , 同时把当前元素num[i]压栈
+ * 可以看到栈内的元素一定是单调递减的， 因为如果有比栈内元素大的值会进行弹出
  *
  * @Author: chaoyi.zhang
  * @Date: 2020/10/10 15:19
@@ -43,9 +46,45 @@ public class DailyTemperature {
         return result;
     }
 
+    /**
+     * 单调栈
+     * @param T
+     * @return
+     */
+    public int[] dailyTemperatures3(int[] T) {
+        int[] result = new int[T.length];
+        Arrays.fill(result, 0);
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<T.length;i++){
+            if(stack.isEmpty()){
+                stack.push(i);
+            } else {
+                Integer topIndex = stack.peek();
+                if (T[i] <= T[topIndex]) {
+                    stack.push(i);
+                } else {
+                    //不断进行栈顶元素判断是否满足
+                    do{
+                        topIndex = stack.peek();
+                        if(T[i] <= T[topIndex]){
+                            stack.push(i);
+                            break;
+                        }
+                        stack.pop();
+                        result[topIndex] = i - topIndex;
+                    } while(!stack.isEmpty());
+                    stack.push(i);
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args){
-        int[] array = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] array = {89,62,70,58,47,47,46,76,100,70};
         DailyTemperature dt = new DailyTemperature();
-        System.out.println(Arrays.toString(dt.dailyTemperatures(array)));
+        System.out.println(Arrays.toString(dt.dailyTemperatures3(array)));
     }
 }
