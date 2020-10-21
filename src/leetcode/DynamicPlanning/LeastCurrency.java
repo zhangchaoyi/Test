1b4f9604bf887848884323c959f671eb10b1f3c3
chaoyi.zhang <chaoyi.zhang@za.group>
2020-10-21 17:03:59 +0800

    零钱兑换-完全背包

--- src/leetcode/DynamicPlanning/LeastCurrency.java
+++ src/leetcode/DynamicPlanning/LeastCurrency.java
@@ -3,7 +3,7 @@ package leetcode.DynamicPlanning;
 import java.util.Arrays;
 
 /**
- * 322. 零钱兑换
+ * 322. 零钱兑换, 类似于完全背包，套公式取最小值
  * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
  *
  * 你可以认为每种硬币的数量是无限的。
@@ -36,9 +36,9 @@ import java.util.Arrays;
  * 1 <= coins[i] <= 2^31 - 1
  * 0 <= amount <= 10^4
  * =============================================================================================================================
- * 官方思路：
+ * 官方思路：类似完全背包
  * dp[i] 表示组成i元的最少货币数, 则考虑最后一张组成的货币可能是arr[0..j]的其中一种
- * dp[i] = min{dp[i-coins[j]] + 1， (0<=j<=coins.length && i>=coins[j])} , 表示dp[i]由所有最后一张货币的情况中的最小值 +1 ；
+ * dp[i] = min{dp[i], dp[i-coins[j]] + 1， (0<=j<=coins.length && i>=coins[j])} , 表示dp[i]由所有最后一张货币的情况中的最小值 +1 ；
  * dp[length-1]即所求值
  * dp[0]=0
  *
@@ -84,18 +84,25 @@ import java.util.Arrays;
 public class LeastCurrency {
 
     public static void main(String[] args){
-        int[] arr=new int[]{5,2,3};
+        int[] arr=new int[]{2};
         int aim=0;
 
         LeastCurrency lc = new LeastCurrency();
-        int[] dp = lc.getDp(arr, aim);
-        System.out.println(Arrays.toString(dp));
-        System.out.println(dp[dp.length-1]);
+//        int[] dp = lc.getDp(arr, aim);
+//        System.out.println(Arrays.toString(dp));
+//        System.out.println(dp[dp.length-1]);
+//        LongestCommonSubstring.printArray(lc.getDp2(arr, aim));
+        System.out.println(lc.coinChange2(arr, aim));
     }
 
     public int coinChange(int[] coins, int amount){
         int[] dp = getDp(coins, amount);
         return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public int coinChange2(int[] coins, int amount){
+        int[][] dp = getDp2(coins, amount);
+        return dp[coins.length-1][amount] > amount ? -1 : dp[coins.length-1][amount];
     }
 
     /**
@@ -116,6 +123,35 @@ public class LeastCurrency {
                     dp[i] = Math.min(dp[i], dp[i-arr[j]] + 1);
                 } else {
                     //当前货币面额比所求组成金额大，说明当前情况不满足
+                }
+            }
+        }
+        return dp;
+    }
+
+    public int[][] getDp2(int[] c, int V){
+        int[][] dp = new int[c.length][V+1];
+        int max = (int) Math.pow(10, 4)+1;
+        //init dp[0][0]=0
+        //以下为第一行和第一列初始
+        //dp[0][j]表示只取第一个物品组成[0..j]的最小值，如果满足面额要求都是 j/c[0]
+        for(int j=0;j<V+1;j++){
+            if(c[0]<=j && j%c[0]==0){
+                dp[0][j]=j/c[0];
+            } else {
+                dp[0][j]=max;
+            }
+        }
+        //dp[i][0] 表示从[0...i]种货币组成面额为0的物品的价值, 都是0  -- 根据题目题意
+        for(int i=0;i<c.length;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1;i<c.length;i++){
+            for(int j=1;j<V+1;j++){
+                if (j>=c[i]) {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-c[i]]+1);
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
