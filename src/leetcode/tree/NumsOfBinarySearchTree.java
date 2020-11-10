72d4c471bf91d3fde37285f8cdcc95e680550ce5
chaoyi.zhang <chaoyi.zhang@za.group>
2020-11-10 15:51:09 +0800

    添加官方解答二叉树排序树

--- src/leetcode/tree/NumsOfBinarySearchTree.java
+++ src/leetcode/tree/NumsOfBinarySearchTree.java
@@ -3,55 +3,51 @@ package leetcode.tree;
 /**
  * 96. 不同的二叉搜索树
  * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: 3
  * 输出: 5
  * 解释:
  * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
  *
- *    1         3     3      2      1
- *     \       /     /      / \      \
- *      3     2     1      1   3      2
- *     /     /       \                 \
- *    2     1         2                 3
  * @Author: chaoyi.zhang
  * @Date: 2020/11/9 10:47
- *
- * todo
- *
+ * <p>
+ * *参考官方思路*
+ * <p>
+ * 解题思路
+ * 标签：动态规划
+ * 假设 n 个节点存在二叉排序树的个数是 G (n)，令 f(i) 为以 i 为根的二叉搜索树的个数，则
+ * G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)
+ * <p>
+ * 当 i 为根节点时，其左子树节点个数为 i-1 个，右子树节点为 n-i，则
+ * f(i) = G(i-1)*G(n-i)
+ * <p>
+ * 综合两个公式可以得到 卡特兰数 公式
+ * G(n) = G(0)*G(n-1)+G(1)*G(n-2)+...+G(n-1)*G(0)
  */
 public class NumsOfBinarySearchTree {
 
     public int numTrees(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        if(n==2) {
-            return 2;
-        }
-        if(n==3) {
-            return 5;
-        }
-        //dp[i] = dp[i-1] * 2 + dp[i-2]
-        int[] dp = new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=5;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i=4;i<n+1;i++){
-            dp[i] = dp[i-1]*2 + dp[i-2];
-        }
+        for (int i = 2; i < n + 1; i++)
+            for (int j = 1; j < i + 1; j++)
+                dp[i] += dp[j - 1] * dp[i - j];
 
         return dp[n];
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         NumsOfBinarySearchTree nbst = new NumsOfBinarySearchTree();
         System.out.println(nbst.numTrees(1));
     }
