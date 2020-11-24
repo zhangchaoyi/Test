d5bc1eb7376a75a49fd0d6575229040f5c1a5ab2
chaoyi.zhang <chaoyi.zhang@za.group>
2020-11-24 17:05:40 +0800

    done DiameterOfBinaryTree

--- src/leetcode/array/two_dimension/ReconstructQueue.java
+++ src/leetcode/array/two_dimension/ReconstructQueue.java
@@ -2,6 +2,8 @@ package leetcode.array.two_dimension;
 
 import leetcode.DynamicPlanning.LongestCommonSubstring;
 
+import java.util.*;
+
 /**
  * 406. 根据身高重建队列
  * 假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
@@ -17,7 +19,8 @@ import leetcode.DynamicPlanning.LongestCommonSubstring;
  * 输出:
  * [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
  *
- * todo
+ * 思路：1.bfs 超出时间限制
+ * 2.取巧 [未想明白]
  *
  * @Author: chaoyi.zhang
  * @Date: 2020/11/10 14:23
@@ -25,14 +28,130 @@ import leetcode.DynamicPlanning.LongestCommonSubstring;
 public class ReconstructQueue {
 
     public static void main(String[] args){
-        int[][] array = new int[8][5];//{{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
-        array[7][0]=1;
-        array[4][4]=1;
-        array[7][1]=1;
-        array[5][0]=1;
-        array[6][1]=1;
-        array[5][2]=1;
+        ReconstructQueue rq = new ReconstructQueue();
+//        int[][] array = new int[8][5];//{{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+//        array[7][0]=1;
+//        array[4][4]=1;
+//        array[7][1]=1;
+//        array[5][0]=1;
+//        array[6][1]=1;
+//        array[5][2]=1;
+        int[][] array = new int[][]{{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+        int[][] result = rq.reconstructQueue(array);
 
-        LongestCommonSubstring.printArray(array);
+        LongestCommonSubstring.printArray(result);
+
+//
+//        List<Pair> curList = new ArrayList<>();
+//        curList.add(new Pair(5,0));
+//        curList.add(new Pair(7,0));
+//        System.out.println(rq.canAdd(curList, new Pair(5,1)));
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+        //二维数组多条件排序，按第一列倒序，第二列升序  (第二列升序的原因是防止arrayList插入时下标报错)
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        //排序后的数组，按第二列进行插入排序
+        List<int[]> ans = new ArrayList<int[]>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+    public int[][] reconstructQueue1(int[][] people) {
+        List<Pair> input = new ArrayList<>();
+        for(int i=0;i<people.length;i++){
+            input.add(new Pair(people[i][0], people[i][1]));
+        }
+
+        List<Pair> res = null;
+        Queue<Job> jobQueue = new LinkedList<>();
+        Job firstJob = new Job(new ArrayList<>(), input);
+        jobQueue.add(firstJob);
+
+        while(!jobQueue.isEmpty()){
+            Job job = jobQueue.poll();
+            //跳出条件得到结果
+            if(job.getRemainList().size()==0){
+                res = job.getCurList();
+                break;
+            }
+            //取出job中的curList和remainList 进行判断是否可以插入curList
+            for(int i=0;i<job.getRemainList().size();i++){
+                if(canAdd(job.getCurList(), job.getRemainList().get(i))){
+                    List<Pair> newCurList = new ArrayList<>(job.getCurList());
+                    newCurList.add(job.getRemainList().get(i));
+                    List<Pair> newRemainList = new ArrayList<>(job.getRemainList());
+                    newRemainList.remove(i);
+                    jobQueue.add(new Job(newCurList, newRemainList));
+                }
+            }
+
+        }
+
+        int[][] result = new int[people.length][2];
+        for(int i=0;i<res.size();i++){
+            result[i][0] = res.get(i).getLeft();
+            result[i][1] = res.get(i).getRight();
+        }
+        return result;
+    }
+
+    private boolean canAdd(List<Pair> curList, Pair nextPair){
+        //判断大于nextPair.left的元素有right个
+        int amount = 0;
+        for(int i=0;i<curList.size();i++) {
+            if(curList.get(i).getLeft() >= nextPair.getLeft()){
+                amount++;
+            }
+        }
+        return amount==nextPair.getRight();
+    }
+
+    static class Pair{
+        private int left;
+        private int right;
+
+        Pair(int left, int right){
+            this.left = left;
+            this.right = right;
+        }
+
+        int getLeft(){
+            return this.left;
+        }
+
+        int getRight(){
+            return this.right;
+        }
+    }
+
+    static class Job{
+        private List<Pair> curList;
+        private List<Pair> remainList;
+
+        List<Pair> getCurList(){
+            return this.curList;
+        }
+
+        List<Pair> getRemainList(){
+            return this.remainList;
+        }
+
+        void setCurList(List<Pair> curList){
+            this.curList = curList;
+        }
+
+        void setRemainList(List<Pair> remainList){
+            this.remainList = remainList;
+        }
+
+        Job(List<Pair> curList, List<Pair> remainList){
+            this.curList = curList;
+            this.remainList = remainList;
+        }
     }
 }
--- src/leetcode/tree/DiameterOfBinaryTree.java
+++ src/leetcode/tree/DiameterOfBinaryTree.java
@@ -1,10 +1,11 @@
 package leetcode.tree;
 
+import java.util.Objects;
+
 /**
  * 543. 二叉树的直径
  * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
- *
- *
+ * 等价最大路径/求最大深度等思路
  *
  * 示例 :
  * 给定二叉树
@@ -14,16 +15,63 @@ package leetcode.tree;
  *         2   3
  *        / \
  *       4   5
- * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ *      / \   \
+ *     6  9    8
+ *    /
+ *   7
  *
- * todo
+ *
+ * 返回 5, 它的长度是路径 [7,6,4,2,5,8]
+ *
+ * 定义贡献指当前节点对父节点的最大路径, 如节点4对父节点的贡献是2；  节点5对父节点贡献是1
+ * 每个节点都可以计算他左和右的贡献和
  *
  * @Author: chaoyi.zhang
  * @Date: 2020/11/10 14:19
  */
 public class DiameterOfBinaryTree {
 
+    private int max=Integer.MIN_VALUE;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        calContribution(root);
+
+        return max<0?0:max;
+    }
+
+    private int calContribution(TreeNode root){
+        if (Objects.isNull(root)) {
+            return 0;
+        }
+        int left = calContribution(root.left);
+        int right = calContribution(root.right);
+
+        max = Math.max(max, left+right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        TreeNode Rl = new TreeNode(2);
+        TreeNode Rr = new TreeNode(3);
+        TreeNode Rll = new TreeNode(4);
+        TreeNode Rlr = new TreeNode(5);
+        TreeNode Rlll = new TreeNode(6);
+        TreeNode Rllr = new TreeNode(9);
+        TreeNode Rlrr = new TreeNode(8);
+        TreeNode Rllll = new TreeNode(7);
+
+        root.left = Rl;
+        root.right = Rr;
+        Rl.left = Rll;
+        Rl.right = Rlr;
+        Rll.left = Rlll;
+        Rll.right = Rllr;
+        Rlll.left = Rllll;
+        Rlr.right = Rlrr;
+
+        DiameterOfBinaryTree dob = new DiameterOfBinaryTree();
+        System.out.println(dob.diameterOfBinaryTree(root));
     }
 }
