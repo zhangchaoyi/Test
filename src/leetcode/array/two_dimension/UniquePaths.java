1823d38250f96e6a29f84c04d6bcfddff124c711
chaoyi.zhang <chaoyi.zhang@za.group>
2020-11-24 19:33:06 +0800

    done 不同路径

--- src/leetcode/array/two_dimension/UniquePaths.java
+++ src/leetcode/array/two_dimension/UniquePaths.java
@@ -27,6 +27,35 @@ package leetcode.array.two_dimension;
  * @Date: 2020/11/10 14:26
  *
  * todo
+ * dp[i][j]表示（i,j）的不同路径
+ * dp[i][j] = dp[i][j-1] + dp[i-1][j]
+ *
  */
 public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        //row=0 第一行
+        for(int i=0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        //col=0 第一列
+        for(int i=0;i<m;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args){
+        UniquePaths up = new UniquePaths();
+        System.out.println(up.uniquePaths(1, 1));
+    }
 }
