348bf686671a16208ba0a773a09a945efd4fa4a5
chaoyi.zhang <chaoyi.zhang@za.group>
2020-12-04 14:58:42 +0800

    done searchWord

--- src/leetcode/array/two_dimension/SearchWord.java
+++ src/leetcode/array/two_dimension/SearchWord.java
@@ -1,5 +1,7 @@
 package leetcode.array.two_dimension;
 
+import java.util.Objects;
+
 /**
  * 79. 单词搜索
  * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
@@ -32,7 +34,64 @@ package leetcode.array.two_dimension;
  * @Author: chaoyi.zhang
  * @Date: 2020/11/10 14:28
  *
- * todo
+ * 思路：dfs O(m*n* word.length)
+ *
+ * 结论：如果在递归不返回boolean会超出时间限制，因此在递归方法要求返回boolean才通过
  */
 public class SearchWord {
+
+    public boolean exist1(char[][] board, String word) {
+        int[][] trace = new int[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(findChar(board, word, i, j, 0, trace)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean findChar(char[][] board, String word, int i, int j, int wordIndex, int[][] trace){
+        int ROW_LIMIT = board.length;
+        int COL_LIMIT = board[0].length;
+        //超过边界
+        if(i>=ROW_LIMIT || j>=COL_LIMIT || i<0 || j<0){
+            return false;
+        }
+        if(trace[i][j]==1){
+            return false;
+        }
+        if (Objects.equals(board[i][j], word.charAt(wordIndex))) {
+            if (Objects.equals(wordIndex, word.length()-1)) {
+                return true;
+            }
+            trace[i][j]=1;
+            boolean result = findChar(board, word, i-1, j, wordIndex+1, trace)
+                    || findChar(board, word, i, j-1, wordIndex+1, trace)
+                    || findChar(board, word, i+1, j, wordIndex+1, trace)
+                    || findChar(board, word, i, j+1, wordIndex+1, trace);
+            trace[i][j]=0;
+            return result;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+
+        char[][] chars = new char[][]{
+                {'A','B','C','E'},
+                {'S','F','E','S'},
+                {'A','D','E','E'}
+        };
+//        char[][] chars = new char[][]{
+//                {'A','B','C','E'},
+//                {'S','F','C','S'},
+//                {'A','D','E','E'}
+//        };
+        SearchWord sw = new SearchWord();
+        System.out.println(sw.exist1(chars, "ABCESEEEFS"));
+    }
 }
