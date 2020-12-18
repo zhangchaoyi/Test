6a2d24691a73d9b8e09d02e1c0044962048b30f2
chaoyi.zhang <chaoyi.zhang@za.group>
2020-12-18 17:31:48 +0800

    调整kArray

--- src/leetcode/array/KAarry.java
+++ src/leetcode/array/KAarry.java
@@ -8,12 +8,19 @@ package leetcode.array;
  * 一般解决办法的思路，遍历一次O(n), 直接寻找到k
  * 最优复杂度 二分 O(logn)
  *
+ * k实际上就是找到array[0]
  * 使用 array[0]作为参照物，如果 array[n/2] > array[0] , 说明 k 位置在array[n/2]右侧
  *                        如果 array[n/2] < array[0] , 说明 k 位置在array[n/2]左侧
+ * todo
  */
 public class KAarry {
 
     private int findK(int[] array){
+        //判断是否没有发生移位
+        if (array[0]<array[array.length-1]) {
+            return 0;
+        }
+
         int left = 0;
         int right = array.length - 1;
         int limit = array[0];
@@ -28,16 +35,19 @@ public class KAarry {
 
             System.out.println("left:" + left + " right:" + right);
         }
-
-        if(left == right){
-            return 0;
+        int end=left;
+        //跟前一个比较，确定边界点
+        if(array[left] < array[left-1]){
+            end=left-1;
         }
-        return left + 1;
+
+        return end+1;
     }
 
     public static void main(String[] args){
-//        int[] array = new int[]{10, 28, 40, 50, 1, 4, 5, 6, 8, 9};
-        int[] array = new int[]{1, 4, 5, 6, 8, 9,10, 28, 40, 50};
+//        int[] array = new int[]{10, 28, 30, 1, 4, 5, 6, 8, 9};
+//        int[] array = new int[]{10, 28, 30, 40, 1, 4, 5, 6, 8, 9};
+        int[] array = new int[]{4, 5, 6, 8, 9, 10, 28, 40, 50, 1, 2};
         KAarry k = new KAarry();
         System.out.println(k.findK(array));
     }
