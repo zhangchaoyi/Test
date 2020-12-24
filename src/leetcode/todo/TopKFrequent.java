dbf443193fbc662f825e644f2ece9bb24b26028f
chaoyi.zhang <chaoyi.zhang@za.group>
2020-12-24 11:08:40 +0800

    done top k

--- src/leetcode/todo/TopKFrequent.java
+++ src/leetcode/todo/TopKFrequent.java
@@ -1,7 +1,6 @@
 package leetcode.todo;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 347. 前 K 个高频元素
@@ -28,7 +27,7 @@ import java.util.Map;
  *
  * 思路：1.使用空间换时间， HashMap记录次数
  *      2.排序
- * todo
+ * toSubmit
  * @Author: chaoyi.zhang
  * @Date: 2020/12/17 14:49
  */
@@ -39,11 +38,44 @@ public class TopKFrequent {
         for(int i=0;i<nums.length;i++){
             int count = countMap.getOrDefault(nums[i], 0);
             countMap.put(nums[i], count+1);
-
-            countMap.entrySet().stream().forEach(entry -> {
-
-            });
         }
-        return null;
+        PriorityQueue<Holder> queue = new PriorityQueue<>(k, Comparator.comparingInt(Holder::getCount));
+
+        countMap.entrySet().stream().forEach(entry->{
+            if(queue.size()<2){
+                queue.add(new Holder(entry.getKey(), entry.getValue()));
+            } else {
+                Holder top = queue.peek();
+                if(entry.getValue() > top.getCount()) {
+                    queue.poll();
+                    queue.add(new Holder(entry.getKey(), entry.getValue()));
+                }
+            }
+        });
+        int[] res = new int[k];
+        for(int i=0;i<res.length;i++){
+            res[i]=queue.poll().element;
+        }
+        return res;
+    }
+
+    static class Holder{
+        private int element;
+        private int count;
+
+        private int getCount(){
+            return count;
+        }
+
+        Holder(int element, int count){
+            this.element = element;
+            this.count = count;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{1,1,1,2,2,3};
+        TopKFrequent tk = new TopKFrequent();
+        System.out.println(Arrays.toString(tk.topKFrequent(nums, 2)));
     }
 }
